64a1a1bb7ce475ddd224b141d64879e6894f47b7
Hafiz-Fast <arhamzeeshan617@gmail.com>
2026-07-08 21:04:56 +0500

    Update API Conventions learned and Partial View Implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  # new and create for "Create"
   def new
     @article = Article.new
   end
@@ -18,6 +19,22 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  # edit and update for "Update"
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
